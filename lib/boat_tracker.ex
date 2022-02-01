9e8cdd16e5fffd9f9b7369e98413ced0dd926e27
ChristianTovar <tovarchristian21@gmail.com>
2022-02-01 12:54:49 -0500

    Initial commit

---
+++ lib/boat_tracker.ex
@@ -0,0 +1,18 @@
+defmodule BoatTracker do
+  @moduledoc """
+  Documentation for BoatTracker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BoatTracker.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/boat_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule BoatTrackerTest do
+  use ExUnit.Case
+  doctest BoatTracker
+
+  test "greets the world" do
+    assert BoatTracker.hello() == :world
+  end
+end
