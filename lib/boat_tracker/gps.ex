2c50da279f232d79f1fc55dc02661c6515ef4bd0
ChristianTovar <tovarchristian21@gmail.com>
2022-02-02 12:55:26 -0500

    Replace IO with Logger

--- lib/boat_tracker/gps.ex
+++ lib/boat_tracker/gps.ex
@@ -1,26 +1,27 @@
 defmodule BoatTracker.GPS do
   use GenServer
+  require Logger
 
   def start_link(state), do: GenServer.start_link(__MODULE__, state)
 
   @impl true
   def init(state) do
-    print_coordinates()
+    log_coordinates()
 
     {:ok, state}
   end
 
   @impl true
   def handle_info(:print, state) do
-    print_coordinates()
+    log_coordinates()
 
     {:noreply, state}
   end
 
-  defp print_coordinates do
+  defp log_coordinates do
     # TODO: Read coordinates
 
-    IO.puts("printing for future coordinates!")
+    Logger.info("Logging for future coordinates!")
     Process.send_after(self(), :print, 1000)
   end
 end
