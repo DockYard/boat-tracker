12552a31e3200766563cf91ebd3d6fee00553b98
ChristianTovar <tovarchristian21@gmail.com>
2022-02-04 16:51:45 -0500

    Add UART coordinates reading

--- lib/boat_tracker/gps.ex
+++ lib/boat_tracker/gps.ex
@@ -6,22 +6,33 @@ defmodule BoatTracker.GPS do
 
   @impl true
   def init(state) do
-    log_coordinates()
+    setup_UART()
 
     {:ok, state}
   end
 
   @impl true
-  def handle_info(:print, state) do
-    log_coordinates()
+  def handle_info({:circuits_uart, _serial_port_id, {:error, reason}}, state) do
+    Logger.info("error: #{inspect(reason)}")
 
     {:noreply, state}
   end
 
-  defp log_coordinates do
-    # TODO: Read coordinates
+  @impl true
+  def handle_info({:circuits_uart, _serial_port_id, data}, state) do
+    Logger.info("coordinates: #{inspect(data)}")
 
-    Logger.info("Logging for future coordinates!")
-    Process.send_after(self(), :print, 1000)
+    {:noreply, state}
+  end
+
+  defp setup_UART do
+    {:ok, pid} = Circuits.UART.start_link()
+
+    :ok =
+      Circuits.UART.open(pid, "ttyAMA0",
+        speed: 9600,
+        active: true,
+        framing: {Circuits.UART.Framing.Line, separator: "\r\n"}
+      )
   end
 end
