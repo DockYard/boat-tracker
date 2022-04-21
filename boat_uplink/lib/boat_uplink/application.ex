9a617b6e9f392f4c9aaabcef2dd39c7c74854895
ChristianTovar <tovarchristian21@gmail.com>
2022-04-21 12:38:14 -0500

    Add uplink nerves app

---
+++ boat_uplink/lib/boat_uplink/application.ex
@@ -0,0 +1,44 @@
+defmodule BoatUplink.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: BoatUplink.Supervisor]
+
+    children =
+      [
+        # Children for all targets
+        # Starts a worker by calling: BoatUplink.Worker.start_link(arg)
+        # {BoatUplink.Worker, arg},
+      ] ++ children(target())
+
+    Supervisor.start_link(children, opts)
+  end
+
+  # List all child processes to be supervised
+  def children(:host) do
+    [
+      # Children that only run on the host
+      # Starts a worker by calling: BoatUplink.Worker.start_link(arg)
+      # {BoatUplink.Worker, arg},
+    ]
+  end
+
+  def children(_target) do
+    [
+      # Children for all targets except host
+      # Starts a worker by calling: BoatUplink.Worker.start_link(arg)
+      # {BoatUplink.Worker, arg},
+    ]
+  end
+
+  def target() do
+    Application.get_env(:boat_uplink, :target)
+  end
+end
