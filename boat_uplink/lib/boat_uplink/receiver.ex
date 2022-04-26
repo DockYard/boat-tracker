b4d0875ed45d09f89a0920ed78766bd6d86ebdfd
ChristianTovar <tovarchristian21@gmail.com>
2022-04-26 15:54:43 -0500

    Add handle_info/2 callback

--- boat_uplink/lib/boat_uplink/receiver.ex
+++ boat_uplink/lib/boat_uplink/receiver.ex
@@ -11,6 +11,16 @@ defmodule BoatUplink.Receiver do
     {:ok, pid}
   end
 
+  # A message should be received on every 'send' since this is the process that started the link.
+  @impl true
+  def handle_info(msg, state) do
+    Logger.info("Received data: #{inspect(msg)}")
+
+    # Once data is received, the nmea sentence should be parsed.
+
+    {:noreply, state}
+  end
+
   defp setup_LoRa do
     {:ok, pid} = LoRa.start_link()
     :ok = LoRa.begin(pid, 433.0e6)
