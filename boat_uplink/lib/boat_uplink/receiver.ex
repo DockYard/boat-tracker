e1853b8c441b3058272380c8f3f6472b223ede0d
ChristianTovar <tovarchristian21@gmail.com>
2022-06-13 11:33:25 -0500

    Improve pattern matching on handle_info/2

--- boat_uplink/lib/boat_uplink/receiver.ex
+++ boat_uplink/lib/boat_uplink/receiver.ex
@@ -11,12 +11,9 @@ defmodule BoatUplink.Receiver do
     {:ok, pid}
   end
 
-  # A message should be received on every 'send' since this is the process that started the link.
   @impl true
-  def handle_info(msg, state) do
-    Logger.info("Received data: #{inspect(msg)}")
-
-    # Once data is received, the nmea sentence should be parsed.
+  def handle_info({:lora, %{packet: data}}, state) do
+    Logger.info("Received : #{inspect(data)}")
 
     {:noreply, state}
   end
