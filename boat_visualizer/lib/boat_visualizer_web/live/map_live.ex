122ee14cb21bf4eaf9eac8279de2d831cc22b3f4
ChristianTovar <tovarchristian21@gmail.com>
2022-08-05 15:11:59 -0500

    Add pubsub support to MapLive

--- boat_visualizer/lib/boat_visualizer_web/live/map_live.ex
+++ boat_visualizer/lib/boat_visualizer_web/live/map_live.ex
@@ -1,9 +1,22 @@
 defmodule BoatVisualizerWeb.MapLive do
   use Phoenix.LiveView
+  alias Phoenix.PubSub
+
+  def mount(_params, _session, socket) do
+    if connected?(socket), do: PubSub.subscribe(BoatVisualizer.PubSub, "markers")
+
+    {:ok, socket}
+  end
 
   def render(assigns) do
     ~H"""
     <div id="map" phx-update="ignore"></div>
     """
+  end
+
+  def handle_info({_latitude, _longitude}, socket) do
+    # send coordinates to leaflet map
+
+    {:noreply, socket}
   end
 end
